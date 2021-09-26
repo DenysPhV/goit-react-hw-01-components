903206a54c9fbb93d12710e53f9c36fb14c5a6e0
Denys Filichkin <denys.philichkin@gmail.com>
2021-09-26 19:37:39 +0300

    change directory

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import StatisticsItem from './StatisticsItem';
+import StatisticsItem from '../StatisticsItem/StatisticsItem';
 import s from './Statistics.module.css';
 
 export default function Statistics({ title, stats }) {
